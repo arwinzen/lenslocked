183ded166f1c1eaadf572b0c213f36a4be217739
arwinzen <arwingoo90@gmail.com>
2022-11-22 22:56:22 +0800

    feat: contact handler

--- main.go
+++ main.go
@@ -6,13 +6,19 @@ import (
 	"os"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to your beautiful site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to your awesome site!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:arwingoo90@gmail.com\">arwingoo90@gmail.com</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Fprintln(os.Stdout, "Hello World!")
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
